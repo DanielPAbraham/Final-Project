f9fb8d0003338ecd3d0bd08234f787836ce30a8d
George Mihov <gmihov001@gmail.com>
2022-05-19 23:22:05 +0000

    Created Login, Register, Dashboard templates

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  const { store, actions } = React.useContext(Context);
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">fITmE</span>
+        </Link>
+        {store.user ? (
+          <div className="ml-auto">
+            <Link to="/demo">
+              <button className="btn btn-primary">Dashboard</button>
+            </Link>
+          </div>
+        ) : (
+          <div className="ml-auto">
+            <Link to="/demo">
+              <button className="btn btn-primary mx-2">Login</button>
+            </Link>
+            <Link to="/demo">
+              <button className="btn btn-primary mx-2">Register</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 };
