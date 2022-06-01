64c32ee22a264b3af47a2bb9f4773562ba010121
Daniel A Perez <danielpabraham97@gmail.com>
2022-06-01 00:41:39 +0000

    my journey

--- src/front/js/pages/Recipes.js
+++ src/front/js/pages/Recipes.js
@@ -4,24 +4,32 @@ import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { Usernav } from "../component/Usernav";
 import { RecipeCard } from "../component/recipeCard";
-import { Form } from "react-bootstrap";
+import { Form, Placeholder } from "react-bootstrap";
+import SearchBar from "material-ui-search-bar";
 
 export const Recipes = () => {
   const { store, actions } = useContext(Context);
   const params = useParams();
+  const [searchInput, setSearchInput] = useState();
 
+  function SearchBar({ placeholder, data }) {
+
+  }
   return (
     <>
       <Usernav />
-      <Form.Label>Search</Form.Label>
-      <Form.Control
-        type="text"
-      />
-      <div>
+
+      {/* <SearchBar
+        value={this.state.value}
+        onChange={(newValue) => this.setState({ value: newValue })}
+        onRequestSearch={() => doSomethingWith(this.state.value)}
+      /> */}
+
+      {/* <div>
         {store.complex.map((item, i) => {
           return <RecipeCard key={i} title={item.title} sourceUrl={item.sourceUrl} image={item.image} />;
         })}
-      </div>
+      </div> */}
     </>
   );
 };
