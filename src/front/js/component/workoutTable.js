b0013b2f7012a9bc19f9e9316a42817a90d39482
Daniel A Perez <danielpabraham97@gmail.com>
2022-06-04 15:45:17 +0000

    final week

--- src/front/js/component/workoutTable.js
+++ src/front/js/component/workoutTable.js
@@ -7,7 +7,7 @@ export const WorkoutTable = () => {
     const { actions, store } = useContext(Context)
 
     const columns = [
-        { field: 'id', headerName: 'Exercise', width: 150 },
+        { field: 'id', headerName: 'Exercise', width: 300 },
         {
             field: 'Sets',
             headerName: 'Sets',
@@ -17,7 +17,7 @@ export const WorkoutTable = () => {
         {
             field: 'Reps',
             headerName: 'Reps',
-            width: 30,
+            width: 100,
             editable: false,
         },
         {
@@ -32,22 +32,29 @@ export const WorkoutTable = () => {
             headerName: 'Notes',
             description: 'This column is for writing notes for your workout.',
             sortable: false,
-            width: 160,
-            valueGetter: (params) =>
-                `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+            editable: true,
+            width: 200,
+            // valueGetter: (params) =>
+            //     `${params.row.firstName || ''} ${params.row.lastName || ''}`,
         },
     ];
 
     const rows = [
-        { id: `${store.workouts.exercise}`, lastName: "Snow", Sets: "3", WeightUsed: 35 },
-        { id: "Bench", lastName: "Lannister", Sets: "3", WeightUsed: 42 },
-        { id: "Chin-Ups", lastName: "Lannister", Sets: "3", WeightUsed: 45 },
-        { id: "Accesory of Choice", lastName: "Stark", Sets: "x", WeightUsed: 16 },
-        { id: 5, lastName: "Targaryen", Sets: "Daenerys", WeightUsed: null },
-        { id: 6, lastName: "Melisandre", Sets: null, WeightUsed: 150 },
-        { id: 7, lastName: "Clifford", Sets: "Ferrara", WeightUsed: 44 },
-        { id: 8, lastName: "Frances", Sets: "Rossini", WeightUsed: 36 },
-        { id: 9, lastName: "Roxie", Sets: "Harvey", WeightUsed: 65 },
+        { id: "Overhead Press", Reps: "5-8", Sets: "3", WeightUsed: 105, Notes: "" },
+        { id: "Chin-Ups (Assisted if needed)", Reps: "5-8", Sets: "2", WeightUsed: 55, Notes: "" },
+        { id: "Rope Tricep Pushdowns", Reps: "10-12", Sets: "4", WeightUsed: 35, Notes: "" },
+        { id: "Lateral Raises", Reps: "12-15", Sets: "3", WeightUsed: 20, Notes: "" },
+        { id: "Hanging Leg Raises", Reps: "8-10", Sets: "3", WeightUsed: "BodyWeight", Notes: "" },
+        { id: "Bulgarian Split Squats", Reps: "8-10", Sets: "4", WeightUsed: 45, Notes: "" },
+        { id: "Romanian Deadlift", Reps: "10-12", Sets: "4", WeightUsed: 185, Notes: "" },
+        { id: "Leg Extensions", Reps: "12-15", Sets: "3", WeightUsed: 85, Notes: "" },
+        { id: "Standing Uni-Lateral Calf Raises", Reps: "10-12", Sets: "4", WeightUsed: 45, Notes: "" },
+        { id: "Farmers Walks", Reps: "20-30sec", Sets: "3", WeightUsed: 60, Notes: "" },
+        { id: "Incline Bench Press", Reps: "5-8", Sets: "3", WeightUsed: 155, Notes: "" },
+        { id: "Weighted Dips", Reps: "5-8", Sets: "3", WeightUsed: 80, Notes: "" },
+        { id: "Barbell Curls", Reps: "8-10", Sets: "3", WeightUsed: 60, Notes: "" },
+        { id: "Hammer Curls", Reps: "10-12", Sets: "3", WeightUsed: 35, Notes: "" },
+        { id: "Rope Face-Pulls", Reps: "12-15", Sets: "4", WeightUsed: 85, Notes: "" },
     ];
 
     return (
@@ -55,8 +62,8 @@ export const WorkoutTable = () => {
             <DataGrid
                 rows={rows}
                 columns={columns}
-                pageSize={4}
-                rowsPerPageOptions={[4]}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
                 checkboxSelection
                 disableSelectionOnClick
             />
--- src/front/js/pages/Profile.js
+++ src/front/js/pages/Profile.js
@@ -11,43 +11,47 @@ export const Profile = (props) => {
   return (
     <>
       <Usernav />
-      <div className="bg2 fullscreen h-100 text-center mt-5 end-0">
-        <div className="bg-transparent card">
-          <div className="container">
-            <div className="container">
-              <h1>My Profile</h1>
+      <div class="container">
+        <div class="row gutters">
+          <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
+            <div class="card h-100">
+              <div class="card-body">
+                <div class="account-settings">
+                  <div class="user-profile">
+                    <div class="user-avatar">
+                      <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Maxwell Admin" />
+                    </div>
+                    <h5 class="user-name">Your Profile</h5>
+                  </div>
+                  <div class="about">
+                    <h5>About</h5>
+                    <input />
+                  </div>
+                </div>
+              </div>
             </div>
-            <br></br>
-            <div className="row gx-5">
-              <div className="col-3 ">
-                <img
-                  src="https://i1.sndcdn.com/avatars-1izkebM3cqeF0hcO-uo8bjQ-t500x500.jpg"
-                  className="img-fluid rounded-circle position-relative top-50 start-50 translate-middle"
-                ></img>
-              </div>
-              <div className="col-4 border border-danger bg-light fs-3">
-                <br></br>
-
-                <ul>
-                  <li>First and Last Name</li>
-                  <li>Gender</li>
-                  <li>Birthday</li>
-                  <li>Units of Measurement</li>
-                  <li>Height</li>
-                  <li>Weight</li>
-                  <li>Email</li>
-                </ul>
-              </div>
-
-              <div className=" col-4 border border-danger bg-light">
-                <br></br>
-                <p className="fs-2">About me:</p>
-                <div className="form-floating ">
-                  <textarea
-                    className="form-control aboutme"
-                    placeholder="Leave a comment here"
-                    id="floatingTextarea2"
-                  ></textarea>
+          </div>
+          <div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
+            <div class="card h-100">
+              <div class="card-body">
+                <div class="row gutters">
+                  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <h6 class="mb-2 text-primary">Personal Details</h6>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="fullName">Full Name</label>
+                      <input type="text" class="form-control" id="fullName" placeholder="Enter full name" />
+                    </div>
+                  </div>
+                </div>
+                <div class="row gutters">
+                  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div class="text-right">
+                      <button type="button" id="submit" name="submit" class="btn btn-secondary">Cancel</button>
+                      <button type="button" id="submit" name="submit" class="btn btn-primary">Update</button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
